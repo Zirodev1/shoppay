7690f8f28e2932a310b2c7c8209719585c2ed64e
Lee <zirodev7@gmail.com>
2023-05-26 11:14:40 -0500

    update

---
+++ components/header/Top.js
@@ -0,0 +1,16 @@
+import styles from "./styles.module.scss";
+
+export default function Top() {
+  return (
+    <div className={styles.top}>
+      <div className={styles.top__container}>
+        <div></div>
+        <ul className={styles.top__list}>
+          <li>
+
+          </li>
+        </ul>
+      </div>
+    </div>
+  )
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,27 @@
-import '@/styles/globals.css'
+import "@/styles/globals.scss";
+import { Provider } from "react-redux";
+import store from "@/store";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistStore } from "redux-persist";
+import Head from "next/head";
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+let persistor = persistStore(store);
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        <title>Hanime Plug</title>
+        <meta name="description" content="Hanime Plug-online shopping service for all of your needs."/>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <Component {...pageProps} />
+        </PersistGate>
+      </Provider>
+    </>
+  );
 }
+
+export default MyApp;
